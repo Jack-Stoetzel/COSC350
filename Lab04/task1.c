7902961a8aeb210496b28b00284be6281ae7ae2d
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-10-06 18:15:26 -0400

    Added isdigit function

--- Lab04/task1.c
+++ Lab04/task1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int isdigit(int c)
+{
+    if(c >= 48 && c <= 57)
+        return 1;
+    else
+        return 0;
+}
 
 int main(int argc, char *argv[])
 {
-
+    
     exit(0);
 }
