d7566e08f014f5b023791daab7926e08424c43f5
Jack Thomas Stoetzel <jstoetzel1@hs101-91506.salisbury.edu>
2019-10-14 23:06:00 -0400

    Wrote openUtmpFile function

--- Lab05/task2.1.c
+++ Lab05/task2.1.c
@@ -11,8 +11,21 @@ task3.c
 #include <fcntl.h>
 #include <string.h>
 
+int openUtmpFile()
+{
+    int ufd;
+    ufd = open("/var/run/utmp", O_RDONLY);
+    if(ufd < 0)
+    {
+        puts("Unable to open the utmp file. \n");
+        exit(1);
+    }
+    return ufd;
+}
+
 int main(int argc, char **argv)
 {
-
+    int ufd = openUtmpFile();
+    printf("The utmp file descriptor is %d. \n", ufd);
     return 0;
 }
