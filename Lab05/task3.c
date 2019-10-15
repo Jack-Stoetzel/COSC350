b2d7b11e35990b2d4aef4ec629e14a2eae10485c
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-10-15 15:11:56 -0400

    Edited header comment format

--- Lab05/task3.c
+++ Lab05/task3.c
@@ -6,15 +6,15 @@ task3.c
 Tue Oct 15 12:00:00 2019
 stuct tm
 {
-    int tm_sec;     seconds after the minute	0-60*
-    int tm_min;  	minutes after the hour	0-59
-    int tm_hour;    hours since midnight	0-23
-    int tm_mday;    day of the month	1-31
-    int tm_mon;     months since January	0-11
-    int tm_year;    years since 1900
-    int tm_wday;    days since Sunday	0-6
-    int tm_yday;    days since January 1	0-365
-    int tm_isdst;   Daylight Saving Time flag
+    int tm_sec;     // Seconds after the minute     0-60
+    int tm_min;  	// Minutes after the hour	    0-59
+    int tm_hour;    // Hours since midnight	        0-23
+    int tm_mday;    // Day of the month	            1-31
+    int tm_mon;     // Months since January	        0-11
+    int tm_year;    // Years since 1900
+    int tm_wday;    // Days since Sunday            0-6
+    int tm_yday;    // Days since January 1	        0-365
+    int tm_isdst;   // Daylight Saving Time flag
 }
 */
 
