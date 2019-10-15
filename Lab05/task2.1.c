83378150492bb98fad16e3d6d4f1f18e6a34fe7b
Jack Thomas Stoetzel <jstoetzel1@hs101-91506.salisbury.edu>
2019-10-14 22:41:53 -0400

    Made main

---
+++ Lab05/task2.1.c
@@ -0,0 +1,18 @@
+/*
+Jack Stoetzel
+Lab 04
+task3.c
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
