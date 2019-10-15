cf49a0d851b689d1748db57a67df5cc5d4586143
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-10-14 21:30:41 -0400

    Made main

---
+++ Lab04/task4.c
@@ -0,0 +1,20 @@
+/*
+Jack Stoetzel
+Lab 04
+task3.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+}
