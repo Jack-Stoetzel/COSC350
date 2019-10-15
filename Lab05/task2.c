edac2e7272c954d977df852c838d39168c495cc1
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-10-15 10:38:03 -0400

    Added file descriptor print out

--- Lab05/task2.c
+++ Lab05/task2.c
@@ -45,6 +45,7 @@ int main(int argc, char* argv[])
     struct utmp *buf;
     int count = 0;
     int ufd = openUtmpFile();
+    printf("The file descriptor of the utmp file is %d.\n", ufd);
 
     // getutent - Reads a line from the current utmp file
     buf = getutent();
