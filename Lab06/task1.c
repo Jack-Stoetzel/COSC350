d558fbc68eb03cde441adca6c6b6e09b1acde9d8
Jack Thomas Stoetzel <jstoetzel1@hs101-01.salisbury.edu>
2019-10-22 11:53:15 -0400

    Initial commit

--- Lab06/task1.c
+++ Lab06/task1.c
@@ -1,13 +1,39 @@
+/*
+Jack Stoetzel
+Lab 06
+Task 1
+task1.c
+DONE
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-char* mygetenv(char* name){
+char* mygetenv(char* name)
+{
+    extern char**  environ;
+    int i, k, valid;
 
-    return NULL;
+    for(i = 0; i < 71; i++)
+    {
+        //printf("%d = %s \n", i, environ[i]);
+        valid = 1;
+        for(k = 0; k < 3; k++)
+        {
+            //printf("%c == %c \n", environ[i][k], name[k]);
+            if(environ[i][k] != name[k])
+            {
+                valid = 0;
+            }
+        }
+        if(valid == 1)
+        {
+            return environ[i];
+        }
+    }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-
+    printf("%s \n", mygetenv(argv[1]));
     return 0;
 }
