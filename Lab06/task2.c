f9fd21fcf4e204ded534a9b9858fc934ceb28e27
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-10-21 21:28:32 -0400

    Initial commit

---
+++ Lab06/task2.c
@@ -0,0 +1,61 @@
+/*
+Jack Stoetzel
+Lab 06
+task2.c
+
+*/
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int myatoi(char *str)
+{
+	int num = 0;
+	int index = 0;
+
+	while(str[index] != '\0')
+	{
+		num = (num * 10) + (str[index] - 48);
+		index++;
+	}
+	return num;
+}
+
+int main(int argc, char* argv[])
+{
+    int Nc = myatoi(argv[1]);
+    int Np = myatoi(argv[2]);
+    int Tc = myatoi(argv[3]);
+    int Tp = myatoi(argv[4]);
+
+    pid_t pid;
+    char* message;
+    int n, t;
+
+    printf("Fork program starting.\n");
+    pid = fork();
+    switch(pid)
+    {
+        case -1:
+            perror("Fork failed.");
+            exit(1);
+        case 0:
+            message = "This is the child.";
+            n = Nc;
+            t = Tc;
+            break;
+        default:
+            message = "This is the parent.";
+            n = Np;
+            t = Tp;
+            break;
+    }
+
+    for(; n > 0; n--)
+    {
+        puts(message);
+        sleep(t);
+    }
+    exit(0);
+}
