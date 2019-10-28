58e41612d4ccfa1bcadf1a44862b95e55329c17d
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-10-28 18:01:25 -0400

    Added stuff...

--- Lab07/task5.c
+++ Lab07/task5.c
@@ -15,5 +15,27 @@
 
 int main(int argc, char* argv[])
 {
+	int i;
+
+	struct sigaction act;
+	//act.sa_handler = ouch;  
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+
+	//Loop 1: Block SIGINT and SIGQUIT
+	for(i = 1; i <= 5; i++){
+		printf("%d \n", i);
+		sigaction(SIGINT, &act, NULL);
+		sigaction(SIGQUIT, &act, NULL);
+		sleep(1);
+	}
+
+	// Loop 2: Block SIGINT
+	for(i = 1; i <= 5; i++){
+		printf("%d \n", i);
+		sigaction(SIGINT, &act, NULL);
+		sleep(1);
+	}
+
 	return 0;
 }
