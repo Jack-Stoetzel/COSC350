faf8acac564d52e290df024cc153805d208ac438
Jack Thomas Stoetzel <jstoetzel1@hs101-11.salisbury.edu>
2019-10-28 13:08:03 -0400

    Changed to replicate 'ctrlc1.c'

--- Lab07/ctrlc2.c
+++ Lab07/ctrlc2.c
@@ -5,10 +5,12 @@
  ctrlc2.c
 
      struct sigaction {
-        void (*) (int) sa_hadler;   // function, SIG_DFL or SIG_IGN 
-        sigset_t sa_mask;           // signals to block in sa_handler 
+        void (*) (int) sa_hadler;   // function, SIG_DFL or SIG_IGN
+        sigset_t sa_mask;           // signals to block in sa_handler
         int sa_flags;               // signal actoin modifiers
     };
+
+
 
  */
 
@@ -19,18 +21,22 @@
 void ouch(int sig)
 {
     printf("\t OUCH! - I got signal %d. \n", sig);
-    //sigaction(SIGQUIT, &act, NULL);
+
+    // Converts Ctrl+C and Ctrl+\ back to their default operations
+    signal(SIGINT, SIG_DFL);
+    //signal(SIGQUIT, SIG_DFL);
 }
 
 int main(int argc, char* argv[])
 {
     struct sigaction act;
 
-    act.sa_handler = ouch;
+    act.sa_handler = ouch;  // When a signal is used with act, the ouch() function will be called
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
 
-    sigaction(SIGINT, &act, 0);
+    sigaction(SIGINT, &act, NULL);      // Sets Ctrl+C to trigger the ouch() function
+    //sigaction(SIGQUIT, &act, NULL);   // Sets Ctrl+\ to trigger the ouch() function
 
     while(1)
     {
