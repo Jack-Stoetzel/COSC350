35743dc5c1532e13dae452636e86637de5709425
Jack Stoetzel <stoetzeljt@gmail.com>
2019-10-29 13:05:14 -0400

    Initial commit

---
+++ Lab08/task2.c
@@ -0,0 +1,18 @@
+/*
+ Jack Stoetzel
+ Lab 08
+ Task 2
+ task2.c
+
+ */
+
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ Lab08/task4.c
@@ -0,0 +1,18 @@
+/*
+ Jack Stoetzel
+ Lab 08
+ Task 4
+ task4.c
+
+ */
+
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
