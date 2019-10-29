408a882d2ac0312dc06a339b76d568edfdec254d
Jack Stoetzel <stoetzeljt@gmail.com>
2019-10-28 21:07:16 -0400

    Added sigprocmask stuff

--- Lab07/task5.c
+++ Lab07/task5.c
@@ -1,9 +1,15 @@
 /*
- * Jack Stoetzel
- * Lab 07
- * Task 5
- * task5.c
- * 
+ Jack Stoetzel
+ Lab 07
+ Task 5
+ task5.c
+
+ 	int sigprocmask(int how, const sigset_t *set, const sigset_t *oset);
+
+	how options:
+		SIG_BLOCK
+		SIG_UNBLOCK
+		SIG_SETMASK
  */
 
 #include <sys/types.h>
@@ -16,24 +22,37 @@
 int main(int argc, char* argv[])
 {
 	int i;
+	sigset_t sigs, old;
 
-	struct sigaction act;
-	//act.sa_handler = ouch;  
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
+	// Clears the signal set
+	sigemptyset(&sigs);
+
+	// Adds SIGINT to the signal set 'sigs'
+	sigaddset(&sigs, SIGINT);
+
+	// Adds SIGQUIT to the signal set 'sigs'
+	sigaddset(&sigs, SIGQUIT);
+
+	// Blocks the signal in the signal set 'sigs' and store the old signal mask in the set 'old'
+	sigprocmask(SIG_BLOCK, &sigs, &old);
 
 	//Loop 1: Block SIGINT and SIGQUIT
 	for(i = 1; i <= 5; i++){
 		printf("%d \n", i);
-		sigaction(SIGINT, &act, NULL);
-		sigaction(SIGQUIT, &act, NULL);
 		sleep(1);
 	}
+
+	//sigprocmask(SIG_SETMASK, &sigs, &old);
+
+	sigemptyset(&sigs);
+
+	sigaddset(&sigs, SIGINT);
+
+	sigprocmask(SIG_BLOCK, &sigs, &old);
 
 	// Loop 2: Block SIGINT
 	for(i = 1; i <= 5; i++){
 		printf("%d \n", i);
-		sigaction(SIGINT, &act, NULL);
 		sleep(1);
 	}
 
