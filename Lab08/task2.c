3c1a73b458a66feabc325a48d626e24a0296bf86
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-11-04 09:50:29 -0500

    Added calcAvgMed function

--- Lab08/task2.c
+++ Lab08/task2.c
@@ -15,22 +15,47 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void readScores(float* scores)
+void readScores(int* scores)
 {
-	int i, valid = 1;
+	int i, score;
+	int valid = 1;
 
 	puts("You may enter up to 20 test scores, or enter -1 to quit early.");
 
 	for(i = 0; i < 20 && valid; i++)
 	{
 		printf("Enter test score %d: ", i + 1);
-		scanf("%f", scores[i]);
-		
+		scanf("%d", &score);
+		scores[i] = score;
+
 		if(scores[i] == -1)
 		{
 			valid = 0;
 		}
+
 	}
+
+	pthread_exit(NULL);
+}
+
+void calcAvgMed(int* scores)
+{
+	int i;
+	int total = 0;
+
+	for(i = 0; scores[i] != -1; i++)
+	{
+		total += scores[i];
+	}
+	
+	if(i == 0)
+	{
+		puts("The average and median are 0 becuase there were no scores entered.");
+		pthread_exit(NULL);
+	}
+
+	float avg = (float) total / i;
+
 	pthread_exit(NULL);
 }
 
@@ -39,13 +64,22 @@ int main(int argc, char* argv[])
 	pthread_t THREADS[4];
 	int rc, i;
 
-	float* scores = malloc(20 * sizeof(float));
+	int* scores = malloc(20 * sizeof(int));
 	for(i = 0; i < 20; i++)
 	{
 		scores[i] = -1;
 	}
 
 	rc = pthread_create(&THREADS[0], NULL, readScores, scores);
+	if(rc)
+	{
+		puts("Error ocurred when creating pthread for reading test scores.");
+		exit(-1);
+	}
+
+	pthread_join(THREADS[0], NULL);
+
+	rc = pthread_create(&THREADS[1], NULL, calcAvgMed, scores);
 	if(rc)
 	{
 		puts("Error ocurred when creating pthread for reading test scores.");
