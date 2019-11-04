f08c8a6575e23668e7147cd2ee07071d8de0c894
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-11-03 19:50:21 -0500

    Made the main and doMath function

--- Lab08/task1.c
+++ Lab08/task1.c
@@ -4,15 +4,42 @@
  Task 1
  task1.c
 
+	Take in 2 command line arguments.
+	Make 2 threads
+		1. One thread finds the sum from 1 - n
+		2. The other finds the product from 1 - n
+
  */
 
-#include <sys/types.h>
-#include <signal.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+
+void *doMath(void *threadID){
+	int ID = (int) threadID;
+	printf("My thread ID is %d \n", ID);
+	if(ID == 0){
+
+	}
+	if(ID == 1){
+
+	}
+	pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[])
 {
+	pthread_t THREADS[2];
+	int currentThread, i;
+	int num = myatoi(argv[1]);
+	for(i = 0; i < 2; i++){
+		currentThread = pthread_create(&THREADS[i], NULL, doMath, (void *) i, num);
+		if(currentThread){
+			printf("Error! pthread_create returned the error code %d \n", currentThread);
+			exit(-1);
+		}
+	}
+	pthread_exit(NULL);
     return 0;
 }
