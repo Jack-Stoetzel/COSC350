41f45e354838c51c4d75d3298660ada4e849df83
Jack Stoetzel <stoetzeljt@gmail.com>
2019-11-05 10:01:15 -0500

    Temporary commit

--- Lab08/task3.c
+++ Lab08/task3.c
@@ -1,37 +1,66 @@
 /*
-Jack Stoetzel
-Lab 08
-Task 3
-task3.c
+ Jack Stoetzel
+ Lab 08
+ Task 3
+ task3.c
 
-*/
+ */
 
-#include <stdlib.h>
+#include<unistd.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
 
-/* Read characters from the pipe and echo them to stdout. */
-void read_from_pipe (int fd)
+#define MAXLINE 256
+
+void err_sys(char *);
+
+int main()
 {
-    FILE *stream;
-    int c;
-    stream = fdopen (fd, "r");
-    while ((c = fgetc (stream)) != EOF) /* Read a character by character until EOF */
-    putchar (c); /* Write on standard output */
-    fclose (stream);s
-}
-/* Write some random text to the pipe. */
-void write_to_pipe (int fd)
-{
-    FILE *stream;
-    stream = fdopen (fd, "w"); /* open a pipe for writing */
-    fprintf (stream, "hello, world!\n");
-    fprintf (stream, "goodbye, world!\n");
-    fclose (stream);
+	int n, fd[2], size, int1, int2;
+	pid_t pid;
+	char sline[MAXLINE], rline[MAXLINE];
+
+	if (pipe(fd) < 0)
+		err_sys("pipe error");
+
+	if ( (pid= fork()) < 0)
+		err_sys("fork error");
+	else if (pid> 0)
+	{
+		close(fd[0]);
+		printf("submit two integers\n");
+		while( (size = read(STDIN_FILENO, sline, MAXLINE)) > 0)
+		{
+			write(fd[1], sline, size); /*write to a pipe */
+            waitpid(&pid);
+			printf("submit two integers\n");
+		}
+	}
+    else
+	{
+		close(fd[1]);
+		while((n = read(fd[0], rline, MAXLINE))>0)
+		{
+			if(sscanf(rline, "%d%d", &int1, &int2) == 2)
+			{
+				sprintf(rline, "The sum is %d\n", int1 + int2);
+				n = strlen(rline);
+				if (write(STDOUT_FILENO, rline, n) != n)
+					err_sys("write error");
+			}
+			else
+			{
+				if (write(STDOUT_FILENO, "invalid args\n", 13) != 13)
+					err_sys("write error");
+			}
+		}
+	}
+	return 0;
 }
 
-int main(int argc, char* argv[])
+void err_sys(char *str)
 {
-    return 0;
+	printf("%s \n",str);
+	exit (1);
 }
