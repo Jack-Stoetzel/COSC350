cff95799855cafd27c904259ff08942398c75f43
Jack Stoetzel <stoetzeljt@gmail.com>
2019-11-05 13:28:16 -0500

    Initial commit

---
+++ Lab09/pipe3.c
@@ -0,0 +1,18 @@
+/*
+ Jack Stoetzel
+ Lab 09
+ Task 2
+ pipe3.c
+
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
---
+++ Lab09/pipe4.c
@@ -0,0 +1,18 @@
+/*
+ Jack Stoetzel
+ Lab 09
+ Task 2
+ pipe4.c
+
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
