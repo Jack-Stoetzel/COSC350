b8c602129f0f5f4be0b74fceb3f8be9aea749c7b
Jack Stoetzel <stoetzeljt@gmail.com>
2019-11-05 10:51:04 -0500

    DONE

--- Lab08/task4.c
+++ Lab08/task4.c
@@ -9,9 +9,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
+    int i;
+    int size = 0;
+    char *cmd;
+    for(i = 1; i < argc; i++)
+    {
+        size += strlen(argv[i]);
+    }
+
+    cmd = calloc(size, sizeof(char));
+
+    for(i = 1; i < argc; i++)
+    {
+        sprintf(cmd, "%s %s", cmd, argv[i]);
+    }
+
+    char buf[BUFSIZ];
+    FILE *ptr;
+    if ((ptr = popen(cmd, "r")) != NULL)
+    {
+        while (fgets(buf, BUFSIZ, ptr) != NULL)
+        {
+            (void) printf("%s", buf);
+        }
+    }
+
+    pclose(ptr);
+
     return 0;
 }
