37a5dbe93d09ec5127e9f326c75708af39029018
Jack Stoetzel <Jack-Stoetzel@users.noreply.github.com>
2019-11-11 09:48:05 -0500

    Added code from textbook

--- Lab09/pipe4.c
+++ Lab09/pipe4.c
@@ -11,8 +11,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
+  int data_processed;
+  char buffer[BUFSIZ+1];
+  int file_descriptor;
 
-    return 0;
+  memset(buffer, '\0', sizeof(buffer));
+  sscanf(argv[1], "%d", &file_descriptor);
+  data_processed=read(file_descriptor, buffer,BUFSIZ);
+  
+  printf("%d - read %d bytes: %s\n", getpid(), data_processed, buffer);
+  exit(EXIT_SUCCESS);
 }
