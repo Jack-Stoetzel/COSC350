34a0f4f15613bd0ec38d9a0c6ddaae734ee01fb3
Jack Stoetzel <stoetzeljt@gmail.com>
2019-11-19 13:14:18 -0500

    Updated headers

---
+++ Lab10/msgQsnd.c
@@ -0,0 +1,21 @@
+/*
+ Jack Stoetzel
+ Lab 10
+ Task 1
+ msgQsnd.c
+
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
---
+++ Lab10/task3.c
@@ -0,0 +1,21 @@
+/*
+ Jack Stoetzel
+ Lab 10
+ Task 3
+ task3.c
+
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
