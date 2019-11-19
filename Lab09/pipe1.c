fa43998dcebebf9528f10a70408d50aaa51d77b7
Jack Stoetzel <stoetzeljt@gmail.com>
2019-11-18 21:06:44 -0500

    Done

--- Lab09/pipe1.c
+++ Lab09/pipe1.c
@@ -11,8 +11,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define READ_END = 0;
-#define WRITE_END = 1;
+const int READ_END = 0;
+const int WRITE_END = 1;
 
 int main(int argc, char* argv[])
 {
@@ -21,19 +21,21 @@ int main(int argc, char* argv[])
     const char some_data[] = "123";
     char* buffer = malloc(3 * sizeof(char));
 
-    memset(buffer, '\0', sizeof(buffer));
+    memset(buffer, '\0', 3);
 
     if(pipe(file_pipes) == 0)
     {
-        data_processed = write(file_pipes[1], some_data, strlen(some_data));
+        data_processed = write(file_pipes[WRITE_END], some_data, strlen(some_data));
 
         printf("Wrote %d bytes. \n", data_processed);
 
-        data_processed = read(file_pipes[0], buffer, BUFSIZ);
+        data_processed = read(file_pipes[READ_END], buffer, 3);
 
         printf("Read %d bytes: %s \n", data_processed, buffer);
 
+        free(buffer);
         exit(EXIT_SUCCESS);
     }
+    free(buffer);
     exit(EXIT_FAILURE);
 }
--- Lab09/pipe3.c
+++ Lab09/pipe3.c
@@ -16,15 +16,15 @@ int main()
     int data_processed;
     int file_pipes[2];
     const char some_data[] = "123";
-    char buffer[BUFSIZ+1];
+    char buffer[BUFSIZ + 1];
     pid_t fork_result;
 
-    memset(buffer,'\0',sizeof(buffer));
+    memset(buffer, '\0' ,sizeof(buffer));
 
-    if(pipe(file_pipes)==0)
+    if(pipe(file_pipes) == 0)
     {
         fork_result = fork();
-    	if(fork_result==-1)
+    	if(fork_result == -1)
     	{
         	fprintf(stderr, "Fork Failure");
         	exit(EXIT_FAILURE);
@@ -32,19 +32,18 @@ int main()
     	
     	if(fork_result==0)
     	{
-    		char wBuffer[BUFSIZ +1];
+    		char wBuffer[BUFSIZ + 1];
         	sprintf(buffer, "%d", file_pipes[0]);
         	sprintf(wBuffer, "%d", file_pipes[1]);
-        	sprintf()
        		(void)execl("pipe4","pipe4", buffer, wBuffer, (char*)0);
     	    exit(EXIT_FAILURE);
     	}
     	else
     	{
-        	data_processed=write(file_pipes[1],some_data, strlen(some_data));
+        	data_processed=write(file_pipes[1], some_data, strlen(some_data));
         	close(file_pipes[1]);
         	wait(&fork_result);
-        	printf("%d - wrote %d bytes\n", getpid(), data_processed);
+        	printf("%d - wrote %d bytes \n", getpid(), data_processed);
     	}  
   	}
     exit(EXIT_SUCCESS);
