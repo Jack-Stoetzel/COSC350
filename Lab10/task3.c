1c0dd7ef14907ee12dc768a7f284e9c48e6387c0
Jack Stoetzel <stoetzeljt@gmail.com>
2019-12-03 09:38:30 -0500

    Added Producer & consumer

--- Lab10/task3.c
+++ Lab10/task3.c
@@ -14,6 +14,40 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
+#define N 10
+typedef int semaphore;
+semaphore mutex = 1;
+semaphore empty = N;
+semaphore full = 0;
+
+void producer()
+{
+    int item;
+    while(1)
+    {
+        item = producer_item();
+        down(&empty);
+        down(&mutex);
+        insert_item(item);
+        up(&mutex);
+        up(&full);
+    }
+}
+
+void consumer()
+{
+    int item;
+    while(1)
+    {
+        down(&full);
+        down(&mutex);
+        item = remove_item();
+        up(&mutex);
+        up(&empty);
+        consume_item(item);
+    }
+}
+
 int main(int argc, char* argv[])
 {
 
